1a9740e88da255f38e54a8ad6ba3fbe6424770b6
colewilliam-s <colewilliams0614@gmail.com>
2025-09-29 18:02:20 -0400

    Added an actual way to contact me from my portfolio using JS and EmailJS. Also styled it with a ton new lines in CSS. Changed the way the projects in index.html look on mobile (text is now beneath the image, and the images are bigger) as per the recommendation of Murat Gungor (PhD, UVM). Going to be working on a LoFi music player next.

---
+++ js/contact.js
@@ -0,0 +1,16 @@
+window.addEventListener("DOMContentLoaded", () => {
+    (function(){
+        emailjs.init("hflJtvdT498YNAjzM");
+    })();
+    const form = document.getElementById("form-contact");
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+    emailjs.sendForm("service_ijlzlwc", "template_k6tjemp", form)
+        .then(() => {
+            alert("Message sent successfuly!");
+            form.reset();
+        }, (err) => {
+                alert("Failed to send message. " + JSON.stringify(err));
+        });
+    });
+});

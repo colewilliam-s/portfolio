4ec3939a5801d612852e1edd520557ecfe6325c6
colewilliam-s <colewilliams0614@gmail.com>
2025-10-12 20:30:26 -0400

    created some new JS files that take people to either the home page or the skills and contact page, as well as centered the text for the buttons inside of the nav bar. I'm gonna start working on the music player next, but that will probably be the last thing i do with the nav bar

---
+++ js/goToHome.js
@@ -0,0 +1,3 @@
+document.getElementById("go-to-home").addEventListener("click", ()=>{
+    window.location.href = "../index.html"
+});
---
+++ js/goToSkills.js
@@ -0,0 +1,3 @@
+document.getElementById("go-to-skills").addEventListener("click", ()=>{
+    window.location.href = "html-files/skills-contact.html"
+});

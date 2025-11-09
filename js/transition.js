7662463933b6c5c15376d322fa7213b7714fce24
colewilliam-s <colewilliams0614@gmail.com>
2025-11-08 21:07:45 -0500

    there was an issue where all anchor elements caused the animation, but i made it so only the ones where you changed html pages activate the animation

--- js/transition.js
+++ js/transition.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const ease = "power4.inOut";
 
-    document.querySelectorAll("a").forEach((link) =>{
+    document.querySelectorAll(".nav-bar-buttons").forEach((link) => {
         link.addEventListener('click', (event) =>{
             event.preventDefault();
             const href = link.getAttribute("href");
 
+            console.log('please go twin')
             if(href && !href.startsWith('#') && href !== window.location.pathname){
+                console.log('i went twin!')
                 animateTranstion().then(() =>{
                     window.location.href = href;
                 });
@@ -14,16 +16,30 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    document.querySelectorAll('.a').forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const href = link.getAttribute('href');
+
+            if(href && !href.startsWith('#') && href !== window.location.pathname){
+                animateTranstion().then(() => {
+                    window.location.href = href;
+                })
+            }
+        })
+    })
+
     revealTransition().then(() => {
         gsap.set(".block", { visibility: "hidden" });
     })
 
     function revealTransition() {
         return new Promise((resolve) => {
+            console.log('reveal');
             gsap.set('.block', { scaleY: 1});
             gsap.to('.block', {
                 scaleY: 0,
-                duration: .25,
+                duration: .65,
                 stagger:{
                     each: 0.1,
                     from: 'start',
@@ -38,10 +54,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function animateTranstion() {
         return new Promise((resolve) => {
+            console.log('animate')
             gsap.set('.block', { visibility: 'visible', scaleY: 0});
             gsap.to('.block', {
                 scaleY: 1,
-                duration: .25,
+                duration: .65,
                 stagger:{
                     each: 0.1,
                     from: 'start',
